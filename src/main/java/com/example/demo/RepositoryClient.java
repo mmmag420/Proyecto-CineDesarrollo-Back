64d3ff4b95c88317900dfe044f06bd21f3c028c4
mag <albornoz.mateo.3059@eam.edu.co>
2025-10-05 20:58:19 -0500

    APIREST clientes completa

---
+++ src/main/java/com/example/demo/RepositoryClient.java
@@ -0,0 +1,73 @@
+package com.example.demo;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import model.Client;
+
+@Repository
+public class RepositoryClient {
+
+	private final ArrayList<Client> listaClientes;
+
+	public RepositoryClient() {
+		this.listaClientes = new ArrayList<>();
+	}
+	
+	public List<Client> listar() {
+		return new ArrayList<>(listaClientes);
+	}
+	
+	
+	
+	
+	public boolean guardar(Client cliente) {
+		listaClientes.add(cliente);
+		return true;
+	}
+	
+	public Client buscar(String cedula) {
+		for(int i = 0; i < listaClientes.size(); i++) {
+			if(listaClientes.get(i).getCedula().equals(cedula)) {
+				return listaClientes.get(i);
+			}
+		}
+		return null;
+	}
+	
+	public boolean editar(Client cliente) {
+		Client aux = buscar(cliente.getCedula());
+		
+		if(aux != null) {
+            aux.setNombre(cliente.getNombre());
+            aux.setApellido(cliente.getApellido());
+            aux.setEdad(cliente.getEdad());
+            aux.setCiudad(cliente.getCiudad());
+            aux.setEstadoMembresia(cliente.isEstadoMembresia());
+            aux.setCorreo(cliente.getCorreo());
+            aux.setContraseña(cliente.getContraseña());
+            return true;
+		}
+		return false;
+	}
+	
+	public boolean eliminar(Client cliente) {
+		for(int i = 0; i < listaClientes.size(); i++) {
+			if(listaClientes.get(i).getCedula().equals(cliente.getCedula())) {
+				listaClientes.remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public ArrayList<Client> getListaClientes() {
+		return listaClientes;
+	}
+
+
+
+	
+
+}
---
+++ src/main/java/com/example/demo/ServiceClient.java
@@ -0,0 +1,81 @@
+package com.example.demo;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import model.Client;
+
+@Service
+public class ServiceClient {
+
+	private final RepositoryClient repo;
+
+	public ServiceClient(RepositoryClient repo) {
+		this.repo = repo;
+		iniciarBaseQuemada();
+	}
+	
+	private void iniciarBaseQuemada() {
+		Client mateo = new Client("Cliente", 1, "albornoz.mateo.3059@eam.edu.co", "mateo123", "1034290939", "Mateo", "Albornoz", 20, "Armenia", false, null, null);
+		Client sara = new Client("Cliente", 2, "reyes.sara.3050@eam.edu.co", "sara123", "1092290939", "Sara", "Reyes", 19, "Armenia", false, null, null);
+		Client parra = new Client("Cliente", 3, "parra.david.3052@eam.edu.co", "parra123", "1033290939", "Juan", "Parra", 18, "Armenia", false, null, null);
+		Client salome = new Client("Cliente", 4, "trujillo.salome.3051@eam.edu.co", "salome123", "1045290939", "Salome", "Trujillo", 20, "Armenia", false, null, null);
+		
+		guardarCliente(mateo);
+		guardarCliente(sara);
+		guardarCliente(parra);
+		guardarCliente(salome);
+	}
+	
+	public List<Client> listarClientes() {
+		return repo.listar();
+	}
+
+	
+	public boolean guardarCliente(Client cliente) {
+		
+		Client client = repo.buscar(cliente.getCedula());	
+		
+		if(client != null) {
+			return false;
+		}		
+		
+		if(cliente.getEdad() < 18) {
+			return false;
+		}	
+		
+		return repo.guardar(cliente);
+	}
+	
+	public Client buscarCliente(String cedula) {
+		Client cliente = repo.buscar(cedula);
+		if(cliente == null) {
+			return null;
+		}
+		return cliente;
+	}
+	
+	public boolean editarCliente(Client cliente) {
+		boolean editado = repo.editar(cliente);
+		if(!editado) {
+		   return false;
+		}
+		
+		if(cliente.getEdad() <= 0 || cliente.getEdad() < 18) {
+			return false;
+		}
+		
+		return repo.guardar(cliente);
+		
+	}
+	
+	public boolean eliminarCliente(Client cliente) {
+		Client encontrado = repo.buscar(cliente.getCedula());
+		if(encontrado == null) {
+			return false;
+		}
+		return repo.eliminar(cliente);
+	}
+	
+	
+}
