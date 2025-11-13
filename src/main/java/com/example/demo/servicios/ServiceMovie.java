4994d3b55ae9b5c7cbc495a4889dbf75202e32ed
sreyesflorez0 <sreyesflorez0@gmail.com>
2025-11-13 12:49:59 -0500

    Solucione el error del duplicado de peliculas al iniciar siempre el back

--- src/main/java/com/example/demo/servicios/ServiceMovie.java
+++ src/main/java/com/example/demo/servicios/ServiceMovie.java
@@ -25,45 +25,48 @@ public class ServiceMovie {
 	
 	@PostConstruct
 	private void initSampleData() {
-		upsertIfAbsent(new Movie(
-	        "Nadie 2",
-	        "Hutch regresa enfrentándose a nuevos enemigos tras sus vacaciones familiares.",
-	        "Acción, Comedia, Thriller, Crimen",
-	        "Bob Odenkirk",
-	        "Timo Tjahjanto",
-	        "/imagenes/posterNadie2.jpg",
-	        "/imagenes/btnNadie2.png",
-	        "https://www.youtube.com/watch?v=latAzkdZJO4&ab_channel=RoyalFilms",
-	        "1h 55m",
-	        true
-	    ));
-	    
-		upsertIfAbsent(new Movie(
-	            "El Conjuro",
-	            "Los Warren investigan un nuevo caso sobrenatural lleno de terror y misterio.",
-	            "Terror",
-	            "Vera Farmiga, Patrick Wilson",
-	            "James Wan",
-	            "/imagenes/posterConjuro.jpg",
-	            "/imagenes/btnElConjuro.png",
-	            "https://www.youtube.com/watch?v=pZGe0V7_L-Q&ab_channel=RoyalFilms",
-	            "2h 05m",
-	            true
-	    ));
-	    
-		upsertIfAbsent(new Movie(				
-	            "Los Cuatro Fantásticos",
-	            "Un grupo de superhéroes con poderes extraordinarios lucha contra nuevas amenazas.",
-	            "Acción, Aventura, Ciencia Ficción",
-	            "Ioan Gruffudd, Jessica Alba, Chris Evans, Michael Chiklis",
-	            "Matt Shakman",
-	            "/imagenes/poster4Fantasticos.jpg",
-	            "/imagenes/btnCuatroFantasticos.png",
-	            "https://www.youtube.com/watch?v=g-a8Db2xea0&ab_channel=RoyalFilms",
-	            "2h 10m",
-	            true
-	            
-	    ));
+		if (repositorymovie.count() == 0) {
+			upsertIfAbsent(new Movie(
+			        "Nadie 2",
+			        "Hutch regresa enfrentándose a nuevos enemigos tras sus vacaciones familiares.",
+			        "Acción, Comedia, Thriller, Crimen",
+			        "Bob Odenkirk",
+			        "Timo Tjahjanto",
+			        "/imagenes/posterNadie2.jpg",
+			        "/imagenes/btnNadie2.png",
+			        "https://www.youtube.com/watch?v=latAzkdZJO4&ab_channel=RoyalFilms",
+			        "1h 55m",
+			        true
+			    ));
+			    
+				upsertIfAbsent(new Movie(
+			            "El Conjuro",
+			            "Los Warren investigan un nuevo caso sobrenatural lleno de terror y misterio.",
+			            "Terror",
+			            "Vera Farmiga, Patrick Wilson",
+			            "James Wan",
+			            "/imagenes/posterConjuro.jpg",
+			            "/imagenes/btnElConjuro.png",
+			            "https://www.youtube.com/watch?v=pZGe0V7_L-Q&ab_channel=RoyalFilms",
+			            "2h 05m",
+			            true
+			    ));
+			    
+				upsertIfAbsent(new Movie(				
+			            "Los Cuatro Fantásticos",
+			            "Un grupo de superhéroes con poderes extraordinarios lucha contra nuevas amenazas.",
+			            "Acción, Aventura, Ciencia Ficción",
+			            "Ioan Gruffudd, Jessica Alba, Chris Evans, Michael Chiklis",
+			            "Matt Shakman",
+			            "/imagenes/poster4Fantasticos.jpg",
+			            "/imagenes/btnCuatroFantasticos.png",
+			            "https://www.youtube.com/watch?v=g-a8Db2xea0&ab_channel=RoyalFilms",
+			            "2h 10m",
+			            true
+			            
+			    ));
+		}
+		
 
 	}
 	
